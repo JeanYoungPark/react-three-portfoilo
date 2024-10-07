7646cfaaa87f144adb8d007e3ba263047523ef8a
JeanYoungPark <jean.young.park.96@gmail.com>
2024-10-07 16:21:46 +0900

    dev: 포트폴리오 페이지 작업 중

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,5 +1,75 @@
 import React from "react";
+import "../css/portfolio.min.css";
 
 export const Portfolio = () => {
-    return <div>Portfolio</div>;
+    return (
+        <div>
+            <div className='portfolio-container'>
+                <div className='portfolio'>
+                    <div className='cards'>
+                        <div className='card active'>
+                            <div className='inner'>
+                                <div className='text'>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas recusandae, ullam ipsam sed, ipsum voluptatibus,
+                                    repellendus quis illum ut doloribus mollitia aspernatur quas cupiditate at ratione. Amet harum molestiae nisi!
+                                </div>
+                                <div className='link'>https://codepen.io/cobra_winfrey/pen/ZNGQKx</div>
+                            </div>
+                        </div>
+                        <div className='card'>
+                            <div className='inner'>
+                                <div className='text'>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas recusandae, ullam ipsam sed, ipsum voluptatibus,
+                                    repellendus quis illum ut doloribus mollitia aspernatur quas cupiditate at ratione. Amet harum molestiae nisi!
+                                </div>
+                                <div className='link'>https://codepen.io/cobra_winfrey/pen/ZNGQKx</div>
+                            </div>
+                        </div>
+                        <div className='card'>
+                            <div className='inner'>
+                                <div className='text'>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas recusandae, ullam ipsam sed, ipsum voluptatibus,
+                                    repellendus quis illum ut doloribus mollitia aspernatur quas cupiditate at ratione. Amet harum molestiae nisi!
+                                </div>
+                                <div className='link'>https://codepen.io/cobra_winfrey/pen/ZNGQKx</div>
+                            </div>
+                        </div>
+                        <div className='card'>
+                            <div className='inner'>
+                                <div className='text'>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas recusandae, ullam ipsam sed, ipsum voluptatibus,
+                                    repellendus quis illum ut doloribus mollitia aspernatur quas cupiditate at ratione. Amet harum molestiae nisi!
+                                </div>
+                                <div className='link'>https://codepen.io/cobra_winfrey/pen/ZNGQKx</div>
+                            </div>
+                        </div>
+                        <div className='card'>
+                            <div className='inner'>
+                                <div className='text'>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas recusandae, ullam ipsam sed, ipsum voluptatibus,
+                                    repellendus quis illum ut doloribus mollitia aspernatur quas cupiditate at ratione. Amet harum molestiae nisi!
+                                </div>
+                                <div className='link'>https://codepen.io/cobra_winfrey/pen/ZNGQKx</div>
+                            </div>
+                        </div>
+                        <div className='card'>
+                            <div className='inner'>
+                                <div className='text'>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas recusandae, ullam ipsam sed, ipsum voluptatibus,
+                                    repellendus quis illum ut doloribus mollitia aspernatur quas cupiditate at ratione. Amet harum molestiae nisi!
+                                </div>
+                                <div className='link'>https://codepen.io/cobra_winfrey/pen/ZNGQKx</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='info'>
+                <p>Grab or Click the cards!</p>
+                <div className='grab'>
+                    <img src='https://img.icons8.com/?size=100&id=eE2AKkZiuPV0&format=png&color=ffffff' alt='grab' />
+                </div>
+            </div>
+        </div>
+    );
 };
